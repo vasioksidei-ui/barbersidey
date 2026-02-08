eec528403437274aec04dc3521c2261585dc372a
vasioksidei-ui <vasiok.sidei@gmail.com>
2026-02-08 23:23:13 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("bookingForm");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const service = document.getElementById("service").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  const text =
+    `Привет! Хочу записаться:%0A` +
+    `Имя: ${name}%0A` +
+    `Телефон: ${phone}%0A` +
+    `Услуга: ${service}%0A` +
+    `Дата: ${date}%0A` +
+    `Время: ${time}`;
+
+  const url = `https://wa.me/420777067024?text=${text}`;
+
+  window.open(url, "_blank");
+});
